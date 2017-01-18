8c6a4de4522143f6725d23a57b9973a37bf17ee9
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-01-18 17:36:55 -0500

    Adding bunch of methods to base class
    Also beginning some logging functionality

--- python/iteration_mapper.py
+++ python/iteration_mapper.py
@@ -1,41 +1,45 @@
 from qgis.core import *
-
+from qgis.gui import QgsMapCanvas, QgsMapCanvasLayer
+from qgis.PyQt.QtCore import QFileInfo
 from qgis.PyQt.QtXml import QDomDocument
-from qgis.gui import QgsMapCanvasLayer
 
 class IteratingMapper( object ):
     '''Holds settings for iterating over multiple congestion maps
     '''
-    BACKGROUND_LAYERNAMES = None
-    COMPOSER_LABELS = None
+    BACKGROUND_LAYERNAMES = []
+    COMPOSER_LABELS = {}
     
     def __init__(self, logger, dbsettings, stylepath, templatepath, projectfile = None, console = False):
         self.logger = logger
-        self.uri = _new_uri(dbsettings)
-
+        self.uri = self._new_uri(dbsettings)
+        
+        self.logger.info('Loading template')
         self.stylepath = stylepath
-        self.agg_level = agg_level
         self.template = QDomDocument()
         with open(templatepath, 'r') as templateFile:
             templateContent = templateFile.read()
-            self.template.setContent(myTemplateContent)
+            self.template.setContent(templateContent)
         
         self.project = None
         if projectfile:
-            self.project = QgsProject.instance().read(QFileInfo(project_path))
+            raise NotImplementedError('Loading projects causes Python to crash')
+            self.logger.info('Loading project')
+            self.project = QgsProject.instance()
+            self.project.read(QFileInfo(projectfile))
         
+        self.logger.info('Loading print composer')
         printcomposer = self._load_print_composer(console=console)
         self.composition = printcomposer['QgsComposition']
         self.map_settings = printcomposer['QgsMapSettings']
         self.composer_view = printcomposer['QgsComposerView']
         
+        self.logger.info('Setting Map Registry and getting background layers')
         self.map_registry = QgsMapLayerRegistry.instance()
         self.background_layers = self.get_background_layers()
         self.layer = None
+        self.logger.info('Mapper created successfully')
     
-
-    @staticmethod
-    def _new_uri(dbset):
+    def _new_uri(self, dbset):
         '''Create a new URI based on the database settings and return it
 
         Args:
@@ -47,7 +51,7 @@ class IteratingMapper( object ):
         uri.setConnection(dbset['host'], "5432", dbset['database'], dbset['user'], dbset['password'])
         return uri
 
-    def _load_print_composer(self, console=True, template = None):
+    def _load_print_composer(self, console=True, template = None, iface = None):
         '''Load a print composer template from object template or template argument
 
         Args:
@@ -71,16 +75,17 @@ class IteratingMapper( object ):
             composition = composerView.composition()
             mapSettings = composition.mapSettings()
         else:
-            canvas = QgsMapCanvas()
+            #canvas = QgsMapCanvas()
             # Next three lines from http://kartoza.com/en/blog/how-to-create-a-qgis-pdf-report-with-a-few-lines-of-python/
-            bridge = QgsLayerTreeMapCanvasBridge(
-                QgsProject.instance().layerTreeRoot(), canvas)
-            bridge.setCanvasLayers()
-            mapSettings = canvas.mapSettings()
+            #bridge = QgsLayerTreeMapCanvasBridge(
+            #    QgsProject.instance().layerTreeRoot(), canvas)
+            #bridge.setCanvasLayers()
+            #mapSettings = canvas.mapSettings()
+            mapSettings = QgsMapSettings()
             composition = QgsComposition(mapSettings)
             composition.loadFromTemplate(template)
-            map_item = composition.getComposerItemById('map')
-            map_item.setMapCanvas(canvas)
+#            map_item = composition.getComposerItemById('map')
+#            map_item.setMapCanvas(canvas)
         return {'QgsComposition': composition,
                 'QgsMapSettings': mapSettings,
                 'QgsComposerView': composerView}
@@ -109,3 +114,37 @@ class IteratingMapper( object ):
             None'''
         for label_id, label_text in labels_dict.items():
             self.composition.getComposerItemById(label_id).setText(label_text.format(**labels_update))
+    
+    def print_map(self, printpath, filetype = 'png'):
+        '''Print the map to the specified location
+        
+        Args:
+            printpath: string path to print to
+            filetype: ['png','pdf'] determines type of image to print
+        Raises:
+            NotImplementedError: If filetype is not supported
+        '''
+        
+        self.composition.refreshItems()
+        if filetype == 'png':
+            image = self.composition.printPageAsRaster(0)
+            image.save(printpath)
+        elif filetype == 'pdf':
+            self.composition.exportAsPDF(printpath)
+        else:
+            raise NotImplementError('{filetype} is not supported'.format(filetype=filetype))
+    
+    def clear_layer(self):
+        self.map_registry.removeMapLayer(self.layer)
+        self.layer = None
+    
+    def close_project(self):
+        if self.project is not None:
+            self.project.clear()
+            self.project = None
+
+    #TODO: Cleanup, not sure if actually needed
+#    def set_composition_layers(self):
+#        layerslist = [QgsMapCanvasLayer(self.layer)] + self.background_layers
+#        map_item = composition.getComposerItemById('map')
+#        map_item.
