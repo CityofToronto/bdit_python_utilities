211b2f96e41a901a23bc1d3e6f8b2c8b97b03495
radumas <raphael.a.dumas@gmail.com>
2017-10-10 13:51:35 -0400

    Send_mail works

---
+++ email_notifications/notify_email.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+"""Send a notification email from our gmail account
+
+`
+
+"""
+import base64
+from email.mime.text import MIMEText
+
+import httplib2
+from apiclient import discovery
+
+from quickstart import get_credentials
+
+CREDENTIALS = get_credentials()
+HTTP = CREDENTIALS.authorize(httplib2.Http())
+SERVICE = discovery.build('gmail', 'v1', http=HTTP)
+MESSAGES = SERVICE.users().messages()
+
+
+def send_mail(to, sender, subject, message):
+    """Create a message for an email.
+
+    Args:
+        sender: Email address of the sender.
+        to: Email address of the receiver.
+        subject: The subject of the email message.
+        message_text: The text of the email message."""
+
+    msg = MIMEText(message)
+    msg['From'] = sender
+    msg['To'] = to
+    msg['Subject'] = subject
+    body = {'raw': base64.urlsafe_b64encode(msg.as_bytes()).decode()}
+    MESSAGES.send(userId='me', body=body).execute()
