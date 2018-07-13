06a464cd4e4eb5479a2d575ae533a9807e256987
Raphael Dumas <raphael.a.dumas@gmail.com>
2018-07-13 10:16:52 -0400

    Add setup.py for parsing utils

---
+++ parsing_utilities/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='parsing_utilities',
+    version='0.1',
+    description='Functions to help with processing dates and times for command-line scripts',
+    packages=find_packages(),
+    python_requires='>=2.7'
+)
