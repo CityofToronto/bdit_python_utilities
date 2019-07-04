f018c2c03069c5f82e951fadb853827a573eb1ec
Rick Liu <rliu@ip-10-160-2-198.ec2.internal>
2019-07-04 13:50:39 -0400

    finished mapping function

---
+++ charts/vfh_charts.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  2 17:01:30 2019
+
+@author: rliu4
+"""
+from psycopg2 import connect
+import psycopg2.sql as pg
+import pandas.io.sql as pandasql
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.ticker as ticker
+import geopandas as gpd
+import os
+import shapely
+from shapely.geometry import Point
+os.environ["PROJ_LIB"]=r"C:\Users\rliu4\AppData\Local\Continuum\anaconda3\Library\share"
+
+
+class charts:
+    def ttc(con):
+        query = '''
+        
+        SELECT * FROM gis.subway_to
+        
+        '''
+        ttc = gpd.GeoDataFrame.from_postgis(query, con, geom_col='geom')
+        ttc = ttc.to_crs({'init' :'epsg:3857'})
+        
+        for index, row in ttc.iterrows():
+            rotated = shapely.affinity.rotate(row['geom'], angle=-17, origin = Point(0, 0))
+            ttc.loc[index, 'geom'] = rotated  
+        
+        return ttc
+    
+    def chloro_map(con, df, subway, lower, upper, title):
+        df.columns = ['geom', 'values']
+        light = '#d9d9d9'
+
+        fig, ax = plt.subplots()
+        fig.set_size_inches(6.69,3.345)
+         
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.set_axis_off()
+         
+        mpd = df.plot(column='values', ax=ax, vmin=lower, vmax=upper,  cmap = 'YlOrRd', edgecolor = 'w', linewidth = 0.5)
+        if subway == True:
+            ttc_df = charts.ttc(con)
+            line = ttc_df.plot( ax=ax, linewidth =4, color = 'w', alpha =0.6) # ttc subway layer
+            line = ttc_df.plot( ax=ax, linewidth =2, color = 'k', alpha =0.4) # ttc subway layer
+         
+        props = dict(boxstyle='round', facecolor='w', alpha=0)
+        plt.text(0.775, 0.37, title, transform=ax.transAxes, wrap = True, fontsize=7, fontname = 'Libre Franklin SemiBold',
+                verticalalignment='bottom', bbox=props, fontweight = 'bold') # Adding the Legend Title
+         
+        
+        cax = fig.add_axes([0.718, 0.16, 0.01, 0.22]) # Size of colorbar
+         
+        rect = patches.Rectangle((0.76, 0.01),0.235,0.43,linewidth=0.5, transform=ax.transAxes, edgecolor=light,facecolor='none')
+        ax.add_patch(rect)
+        
+        ax.margins(0.1)
+         
+        sm = plt.cm.ScalarMappable(cmap='YlOrRd', norm=plt.Normalize(vmin=lower, vmax=upper))
+        sm._A = []
+        cbr = fig.colorbar(sm, cax=cax)
+        cbr.outline.set_linewidth(0)
+        tick_locator = ticker.MaxNLocator(nbins=2)
+        cbr.locator = tick_locator
+        cbr.update_ticks()
+        cbr.ax.yaxis.set_tick_params(width=0.5)
+        cbr.ax.tick_params(labelsize=6)  # Formatting for Colorbar Text
+        for l in cbr.ax.yaxis.get_ticklabels():
+            l.set_family("Libre Franklin")
+            
+        return fig, ax
+        
+        
+        
+
+
+
+            
